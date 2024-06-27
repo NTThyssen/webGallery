2cb931f37d10148a2636f98542f92a99b7268576
nthyssen <ntt@pluto.markets>
2024-06-27 20:13:53 +0200

    build

--- justjoew/lib/main.dart
+++ justjoew/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      
       theme: ThemeData(
         // This is the theme of your application.
         //
