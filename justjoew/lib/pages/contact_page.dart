fc0038d8003b3fce89b958d5a0a633d731d8e8cc
joesofine <s164177@student.dtu.dk>
2024-07-24 12:47:22 +0200

    contact page

--- justjoew/lib/pages/contact_page.dart
+++ justjoew/lib/pages/contact_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'; // Import for kIsWeb
+import 'package:justjoew/constants/costum_colors.dart';
 import 'package:justjoew/mixins/scaffoldMixin.dart';
+import 'package:justjoew/widgets/custom_header.dart';
+import 'package:url_launcher/url_launcher.dart'; // Import for url_launcher
 
 class ContactPage extends StatefulWidget {
   const ContactPage({super.key});
@@ -9,14 +13,345 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> with BasicMixin {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+
   @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _subjectController.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _launchEmail() async {
+    final emailUri = Uri(
+      scheme: 'mailto',
+      path: 'justjoewjoew@gmail.com',
+      query: 'subject=Hello', // Optional: you can add a default subject
+    );
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    } else {
+      throw 'Could not launch $emailUri';
+    }
+  }
+
   @override
   Widget body() {
-    return const Center(
-      child: Text(
-        "contact page",
-        style: TextStyle(
-          color: Colors.lightBlue,
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isWeb = kIsWeb; // Check if the app is running on the web
+    final buttonColor = Colors.lightBlue.shade300; // Define the button color
+    final snackBarTextStyle = TextStyle(
+      fontWeight: FontWeight.w400,
+      fontFamily: 'SourceCodePro',
+      fontSize: 16,
+      color: Colors.black,
+    ); // Define the SnackBar text style
+
+    return Center(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(
+          horizontal: isWeb ? screenWidth * 0.20 : screenWidth * 0.05, // Larger padding for web
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40),
+            const Center(
+              child: CustomHeaderLarge(text: 'CONTACT'),
+            ),
+            Column(
+              children: [
+                const Text(
+                  "Hello!\n\n"
+                  "If you want to request a commission or have any questions, feel free to reach out through this form or at my email address: \n",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontFamily: 'SourceCodePro',
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+                MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: GestureDetector(
+                    onTap: _launchEmail,
+                    child: const Text(
+                      "justjoewjoew@gmail.com",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'SourceCodePro',
+                        fontSize: 18,
+                        decoration: TextDecoration.underline, // Underline to indicate a link
+                        color: blueThemePrimary,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black, // Specify shadow color and opacity
+                            offset: Offset(2, 2), // Specify shadow offset
+                            blurRadius: 4, // Specify shadow blur radius
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const Text(
+                  "\nLet’s create something amazing together!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontFamily: 'SourceCodePro',
+                    fontSize: 16,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Name ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'SourceCodePro',
+                        fontSize: 16,
+                        color: Colors.lightBlue300,
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            controller: _firstNameController,
+                            decoration: InputDecoration(
+                              labelStyle: const TextStyle(color: Colors.black),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: const OutlineInputBorder(),
+                              enabledBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: const OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.lightBlue900),
+                              ),
+                              errorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.lightBlue.shade700),
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.lightBlue.shade900),
+                              ),
+                              errorStyle: TextStyle(
+                                color: Colors.lightBlue100, // Set error text color
+                                fontWeight: FontWeight.w400,
+                                fontSize: 12,
+                                fontFamily: 'SourceCodePro',
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your first name';
+                              }
+                              return null;
+                            },
+                            style: const TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Email Address ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'SourceCodePro',
+                        fontSize: 16,
+                        color: Colors.lightBlue300,
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelStyle: const TextStyle(color: Colors.black),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: const OutlineInputBorder(),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue900),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue.shade700),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue.shade900),
+                        ),
+                        errorStyle: TextStyle(
+                          color: Colors.lightBlue100, // Set error text color
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          fontFamily: 'SourceCodePro',
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email address';
+                        }
+                        if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                          return 'Please enter a valid email address';
+                        }
+                        return null;
+                      },
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Subject ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'SourceCodePro',
+                        fontSize: 16,
+                        color: Colors.lightBlue300,
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _subjectController,
+                      decoration: InputDecoration(
+                        labelStyle: const TextStyle(color: Colors.black),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: const OutlineInputBorder(),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue900),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue.shade700),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue.shade900),
+                        ),
+                        errorStyle: TextStyle(
+                          color: Colors.lightBlue100, // Set error text color
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          fontFamily: 'SourceCodePro',
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter the subject';
+                        }
+                        return null;
+                      },
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Message ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'SourceCodePro',
+                        fontSize: 16,
+                        color: Colors.lightBlue300,
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _messageController,
+                      decoration: InputDecoration(
+                        labelStyle: const TextStyle(color: Colors.black),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: const OutlineInputBorder(),
+                        enabledBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: const OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue900),
+                        ),
+                        errorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue.shade700),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.lightBlue.shade900),
+                        ),
+                        errorStyle: TextStyle(
+                          color: Colors.lightBlue100, // Set error text color
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          fontFamily: 'SourceCodePro',
+                        ),
+                      ),
+                      maxLines: 5,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your message';
+                        }
+                        return null;
+                      },
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    const SizedBox(height: 36.0),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            // Process data
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(
+                                  'Submitting form',
+                                  style: snackBarTextStyle, // Apply custom text style
+                                ),
+                                backgroundColor: buttonColor, // Set the SnackBar background color to match the button
+                              ),
+                            );
+                          }
+                        },
+                        child: const Text(
+                          'SUBMIT',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600, // Font weight
+                            fontFamily: 'SourceCodePro', // Use the Source Pro font
+                            fontSize: 18, // Adjust font size as needed
+                            color: Colors.black,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightBlue.shade200, // Light blue button color
+                          minimumSize: Size(screenWidth * 0.2, 60), // Responsive width
+                          padding: EdgeInsets.zero, // Remove extra padding
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12), // Adjust the radius for rounded corners
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 50.0), // Adjust bottom space as needed
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
