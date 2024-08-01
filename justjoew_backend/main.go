766c17c5189de1c89791c48680f8b19fb27df92d
nthyssen <ntt@pluto.markets>
2024-08-01 12:34:38 +0200

    added cert to golang server

--- justjoew_backend/main.go
+++ justjoew_backend/main.go
@@ -1,51 +1,50 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net"
+	"context"
+	pb "justjoew/backend/protos"
+	"log"
+	"net"
 
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/reflection"
-
-    pb "justjoew/backend/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type Item struct {
-    ID   int    `json:"id"`
-    Name string `json:"name"`
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 type server struct {
-    pb.UnimplementedGreeterServer
+	pb.UnimplementedGreeterServer
 }
 
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-    log.Printf("Received: %v", in.GetName())
-    return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
-
-
-
-
 
 func main() {
-	
- lis, err := net.Listen("tcp", ":50051")
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-    }
-    s := grpc.NewServer()
-    pb.RegisterGreeterServer(s, &server{})
-    // Register reflection service on gRPC server.
-    reflection.Register(s)
-    log.Printf("Server listening at %v", lis.Addr())
-    if err := s.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %v", err)
-    }
+
+	creds, err := credentials.NewServerTLSFromFile("../certs/server.crt", "../certs/server.key")
+	if err != nil {
+		log.Fatalf("Failed to load TLS credentials: %v", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer(grpc.Creds(creds))
+
+	// Register your service with the server
+	pb.RegisterGreeterServer(s, &server{})
+
+	// Listen on port 50051
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	log.Println("gRPC server is running on port 50051 with TLS")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
-
-
-
-
-	
