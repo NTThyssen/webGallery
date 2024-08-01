2c899c3fe2e02f6d7d23068e2ad0a80c987aaef7
nthyssen <ntt@pluto.markets>
2024-08-01 12:38:11 +0200

    reverted certs usage

--- justjoew_backend/main.go
+++ justjoew_backend/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
-	pb "justjoew/backend/protos"
 	"log"
 	"net"
 
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/reflection"
+
+	pb "justjoew/backend/protos"
 )
 
 type Item struct {
@@ -26,25 +27,16 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 
 func main() {
 
-	creds, err := credentials.NewServerTLSFromFile("../certs/server.crt", "../certs/server.key")
-	if err != nil {
-		log.Fatalf("Failed to load TLS credentials: %v", err)
-	}
-
-	// Create a gRPC server object
-	s := grpc.NewServer(grpc.Creds(creds))
-
-	// Register your service with the server
-	pb.RegisterGreeterServer(s, &server{})
-
-	// Listen on port 50051
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-
-	log.Println("gRPC server is running on port 50051 with TLS")
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	log.Printf("Server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+		log.Fatalf("failed to serve: %v", err)
 	}
 }
