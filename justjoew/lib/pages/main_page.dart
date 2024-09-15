28cb9ce5ee44cc161d8c15ae5eb949d59ccb8953
joesofine <s164177@student.dtu.dk>
2024-09-15 23:11:00 +0200

    refactor

--- justjoew/lib/pages/main_page.dart
+++ justjoew/lib/pages/main_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:justjoew/constants/ImageStrings.dart';
 import 'package:justjoew/mixins/basic_mixin.dart';
+import 'package:justjoew/utils/theme/spacing.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -17,17 +18,22 @@ class _MainPageState extends State<MainPage> with BasicMixin {
   Widget body() {
     // Get the screen width
     double screenWidth = MediaQuery.of(context).size.width;
-    // Calculate the height based on the aspect ratio of the image
-    double containerHeight = screenWidth * _imageAspectRatio;
+    // Check if the screen width is less than a threshold for small screens
+    bool isSmallScreen = screenWidth < AppSpacing.smallscreen; // Example threshold for small screens
+
+    // Determine container height based on the aspect ratio for large screens
+    double containerHeight = isSmallScreen ? 400.0 : screenWidth * _imageAspectRatio;
+    // Set container width for small screens to be screen width (cropping the image)
+    double containerWidth = isSmallScreen ? screenWidth : screenWidth;
 
     return Container(
-      width: screenWidth,
+      width: containerWidth,
       height: containerHeight,
       decoration: BoxDecoration(
         image: DecorationImage(
           image: const AssetImage(ImageStrings.landscapeImage), // Use centralized image path
-          fit: BoxFit.cover, // Fits the image within the container
-          alignment: Alignment.center,
+          fit: isSmallScreen ? BoxFit.cover : BoxFit.fitWidth, // Cover for small screens, fitWidth for large screens
+          alignment: Alignment.center, // Center the image
         ),
       ),
     );
